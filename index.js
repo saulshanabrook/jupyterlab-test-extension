2cc71dd6af262d53abc8e2830a1b1da30ef22b61
Saul Shanabrook <s.shanabrook@gmail.com>
2019-07-19 16:13:10 -0400

    v1.0

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    id: "jupyterlab-test-extension-1.0",
+    autoStart: true,
+    activate: function() {
+      console.log(
+        "JupyterLab extension jupyterlab-test-extension-1.0 is activated!"
+      );
+    }
+  }
+];
